cb0b4d0b9210e69ed08f6fc4699b76c73dda9b7d
Steven Roland <steven@mojave.io>
2016-05-04 21:59:42 -0400

    Modularzing
    - Split the index file up to make development easier.
    - Included ui-router to dynamically load page segments.

--- dev/app/core.js
+++ dev/app/core.js
@@ -1,6 +1,24 @@
 (function() {
   'use strict';
-  angular.module('intuitive', [])
+  angular.module('intuitive', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'template/home.html',
+          controller: 'homeController'
+        })
+        .state('plans', {
+          url: '/plans',
+          templateUrl: 'template/plans.html',
+          controller: 'planController'
+        });
+      $urlRouterProvider
+        .otherwise('/');
+    })
+    .controller('homeController', function($scope) {
+      $scope.name = "beautiful";
+    })
     .controller('planController', function($scope, planService) {
       $scope.planSearch = '';
       planService.getPlans(function(response) {
